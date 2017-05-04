7dbb2ef7d333989bdbb5a28ee5f99ad3e6c80806
John Chang <CHChang810716@gmail.com>
2017-05-05 03:26:39 +0800

    fix sdl config

--- share/UnderGround/SDL2/config.cmake
+++ share/UnderGround/SDL2/config.cmake
@@ -1,7 +1,7 @@
 find_package( sdl2 CONFIG REQUIRED )
-if( ${SDL2_FOUND} )
-    add_custom_target(SDL2_CONFIG)
-endif()
+# if( ${SDL2_FOUND} ) cmake config not support this variable
+add_custom_target(SDL2_CONFIG)
+# endif()
 show_var ( SDL2_INCLUDE_DIRS   )
 show_var ( SDL2_LIBRARIES      )
 show_var ( SDL2_LIBDIR         )
